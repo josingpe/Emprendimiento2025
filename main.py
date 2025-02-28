5ba8a0e56f4c684cfac56ebb3d75aa1acd7e6e67
josingpe <josingpe@gmail.com>
2025-02-28 16:01:19 -0500

    Inicialización de la base de datos

--- main.py
+++ main.py
@@ -188,8 +188,7 @@ def mostrar_login(page):
             expand=True
         )
     )
-# Llamar a la actualización de la tasa al inicio
-    asyncio.create_task(actualizar_tasa())  # Corrección
+
 # Función para mostrar el menú principal
 def mostrar_menu_principal(page):
     page.controls.clear()
